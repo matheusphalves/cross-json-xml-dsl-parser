403a7de7dec00f8dd550ced3d582d1611d6c007f
Matheus Phelipe <matheusphalves@gmail.com>
2021-08-28 15:08:57 -0300

    added code to import external file with grammar rules

--- Compilador.js
+++ Compilador.js
@@ -1,35 +1,8 @@
-const ohm = require('ohm-js');
+const ohm = require ('ohm-js');
+const fs = require ('fs');
 
-const gramatica = ohm.grammar(`
-Comandos {
-    Start = Comentario* Algoritmo Comentario* Definicao? Comentario* Funcao* Demarcacao Funcao* Comentario*
-    Reservada= "imprima" | Tipo
-    Comentario = "/*" Texto "*/"
-    Texto = any+
-    Digito = digit+
-    Nome = ~Reservada letter+ Comentario*
-    Algoritmo = Nome ";"
-    Definicao = "variáveis" Comentario* Var Comentario* "fim-variáveis"
-    Var= Nome ":" Comentario* Tipo  ";"
-    Tipo= ("inteiro" | "real" | "caractere" | "literal" | "lógico") Comentario*
-    Demarcacao = "início" Comandos* "fim"
-    Comandos= Print | Read | If | Atribuicao | Comentario | While | For
-    Print = "imprima" "("  Expressao  ("," Expressao)* ")"
-    Expressao = Expressao Operadores Expressao --Op 
-       | Digito
-       | Nome
-	   | "(" Expressao ")" --Par
-    Operadores= "<" | ">" | "<=" | ">=" | "=" | "!=" | "%" | "+" | "*" | "/" | "-"
-    Read = "leia" "()" ";"
-    If= "se" Expressao "entao" Comandos* ("senão" Comandos* "fim-se")?
-    Atribuicao= Nome ":=" (Expressao | Comandos)* ";"
-    While = "enquanto" Expressao "faça" Comandos* "fim-enquanto"
-    For = "para" Nome "de" Expressao "até" Expressao ("passo" Digito)? "faça" Comandos* "fim-para"
-    Funcao = "função" Nome "(" Var ("," Var)* ")" ":" Tipo Demarcacao
-
-
-}
-`);
+const grammarCode = fs.readFileSync('./grammarCode.ohm');
+const gramatica = ohm.grammar(grammarCode);
 
 const inputs = `
 algoritimo teste;
