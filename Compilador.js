5d5f4a08466a5cd570deea84328767209b0211c7
Matheus Phelipe <matheusphalves@gmail.com>
2021-08-28 19:21:22 -0300

    added Operacao non-terminal at grammar and created basic structure for executeOperation method

--- Compilador.js
+++ Compilador.js
@@ -2,8 +2,8 @@ const ohm = require('ohm-js');
 
 const gramatica = ohm.grammar(`
 Comandos {
-    Start = Comentario* Algoritmo Comentario* Definicao? Comentario* Funcao* Demarcacao Funcao* Comentario*
-
+    Start = Operacao+
+    Operacao = Comentario* Algoritmo Comentario* Definicao? Comentario* Funcao* Demarcacao Funcao* Comentario*
     Reservada= "imprima" | Tipo
     Comentario = "/*" ( alnum |"\\"" | space | "!" | "@" | "#" | "$" | ":"| "(" | ")" | "|" | "&" | "/")* "*/"
     Digito = digit+
@@ -67,3 +67,125 @@ if(resultado.succeeded()){
   console.log("Erro");
   console.log(resultado.message)
 }
+
+const semantica = gramatica.createSemantics();
+
+function executeOperation(){//operação para interpretar comandos inseridos
+    semantica.addOperation('execute',
+    {
+        Start(operacoes){
+            return {...operacoes.execute()}//execução de todos os dos elementos da árvore
+        },
+        Operacao(com1, algo, com2, def, com3, fun1, demar, fun2, com4){
+            //algo.execute();
+            //def.execute();
+            demar.execute();
+        },
+
+        Reservada(nome){
+
+        },
+
+        Comentario(start, content, end){
+
+        },
+
+        Digito(_){ return this.primitiveValue},
+
+        Nome(letras, com){
+            console.log(letras, com)
+        },
+        
+        String(start, nome, end){
+
+        },
+
+        Algoritmo(algoritmo, nome, pv){
+            console.log(algoritmo.sourceString, nome.sourceString, pv.sourceString)
+        },
+        Definicao(startVar, com1, var_, com2, endVar){
+            console.log(startVar.sourceString, endVar.sourceString);
+            var_.execute(); //não terminal
+        },
+
+        Var(nome, twoDots, com, tipo, pv){
+            //console.log(nome.sourceString, twoDots.sourceString, tipo.sourceString, pv.sourceString)
+            console.log(tipo.execute()); //não terminal
+        },
+
+        VarDefinida(nome, twoDots, com, tipo){
+
+        },
+
+        Tipo(tipo, com){
+            console.log(tipo.sourceString);
+        },
+
+        Demarcacao(start, comandos, end){
+            comandos.execute();
+        },
+
+        Comandos(nome){
+            console.log(nome.sourceString);
+        },
+
+        Print(imp, pr1, stringOrExp, virgula, otherExp, pr2, pv){
+
+        },
+
+        Expressao_Op(exp, op, exp2){
+
+        },
+
+        Expressao(comand){//Digito DefFuncao, Nome
+
+        },
+
+        Operadores(op){
+
+        },
+
+        Read(leia, parentesis, pv){
+
+        },
+
+        If(se, exp, entao, comandos, senao, maisComandos, fim){//vulgo inteligencia
+
+        },
+
+        Atribuicao(nome, atrib, expOrCom, pv){
+
+        },
+
+        While(enqt, exp, faca, comds, fim){
+
+        },
+
+        For(para, nome, de, exp, ate, exp2, passo, digito, faca, comandos, fim){
+
+        },
+
+        DefFuncao(nome, pr1, exp, pr2){
+
+        },
+
+        Funcao(fun, nome, pr1, varDef, virgula, varDef2, pr2, twoDots, tipo, demar){
+
+        },
+
+
+        Return(retorne, Exp, pv){
+
+        }
+        
+    })
+}
+
+function generateCodeOperation(){//operação para gerar txt com arquivo.c
+    //gerar código compilado e interpretado...
+}
+
+//Execução das operações
+executeOperation();
+
+console.log(semantica(resultado).execute())
