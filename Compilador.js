ae0b2d6bb2a8a9c8772e32a55cf49764b7c12a6b
Murilo Stodolni <mstodolni@gmail.com>
2021-08-29 17:07:18 -0300

    Start grammar XML

--- Compilador.js
+++ Compilador.js
@@ -2,12 +2,24 @@ const ohm = require('ohm-js');
 
 const gramatica = ohm.grammar(`
 Comandos {
-    
+    Inicio = Comentario* Cabecalho Comentario* Estrutura* Comentario*
+    Cabecalho = "<?xml version=\\"" Informacao "\\" encoding=\\"" Informacao "\\" ?>" 
+    Estrutura = "<" Variavel ">" (Estrutura | Informacao)+ "<\/" Variavel ">"
+    Informacao = letter+
+    Variavel = letter+
+    Comentario = "<!--" alnum* "-->"
 }
 `);
 
 const inputs = `
-
+<?xml version="1.0" encoding="UTF-8" ?>
+<!---As categorias dos estudantes carregado por meses---->
+<class_list>
+   <student>
+      <name>Tanmay</name>
+      <grade>A</grade>
+   </student>
+</class_list>
 `;
 
 const resultado = gramatica.match(inputs);
