c9715e9f1310179c511e3e1aed246bb92795231e
Matheus Phelipe <matheusphalves@gmail.com>
2021-08-30 18:03:13 -0300

    added backEnd 'execute' operation (under dev)

--- Compilador.js
+++ Compilador.js
@@ -3,30 +3,83 @@ const ohm = require('ohm-js');
 const gramatica = ohm.grammar(`
 Comandos {
     Inicio = Comentario* Cabecalho Comentario* Estrutura* Comentario*
-    Cabecalho = "<?xml version=\\"" Informacao "\\" encoding=\\"" Informacao "\\" ?>" 
+    Cabecalho = "<?xml version=\\"" Versao "\\" encoding=\\"" Encoding "\\"?>" 
     Estrutura = "<" Variavel ">" (Estrutura | Informacao)+ "<\/" Variavel ">"
-    Informacao = letter+
-    Variavel = letter+
+    Versao = digit+ ("." digit+)*
+    Encoding = letter+ (alnum+ | any)*
+    Informacao = alnum+
+    Variavel = letter+ (alnum)*
     Comentario = "<!--" alnum* "-->"
 }
 `);
 
 const inputs = `
-<?xml version="1.0" encoding="UTF-8" ?>
-<!---As categorias dos estudantes carregado por meses---->
+<?xml version="1.0" encoding="UTF-8"?>
+`;
+
+
+/*
+<!--As categorias dos estudantes carregado por meses-->
 <class_list>
    <student>
       <name>Tanmay</name>
       <grade>A</grade>
    </student>
 </class_list>
-`;
+
+*/
+
+//
 
 const resultado = gramatica.match(inputs);
 
-if(resultado.succeeded()){
+/*if(resultado.succeeded()){
   console.log("Operações OK");
 }else{
   console.log("Erro");
   console.log(resultado.message)
+}*/
+
+const semantica = gramatica.createSemantics();
+
+//está cagado, consertar gramática
+function backEnd(){
+    semantica.addOperation('execute', {
+        Inicio(com1, cab, com2, est, com3){
+            
+        },
+
+        Cabecalho(start, versao, encod, encodingContent, end){
+        },
+
+
+        Estrutura(start1, variavel, end1, content, start2, variavel2, end2){
+
+        },
+
+        Versao(digito, dot, digito){
+
+        },
+
+        Encoding(seila){
+
+        },
+
+
+        Informacao(info){
+            return info.sourceString;
+        },
+
+        Variavel(seila){
+
+        },
+
+        Comentario(){
+
+        }
+    }
+    )
 }
+
+backEnd();
+console.log(semantica(resultado).execute())
