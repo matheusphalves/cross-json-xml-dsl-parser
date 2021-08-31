db4c9d692cb4ec28b6df2f4817cec6a4d565de6b
Murilo Stodolni <m_campanhol@hotmail.com>
2021-08-30 23:11:46 -0300

    retorn line

--- Compilador.js
+++ Compilador.js
@@ -15,6 +15,8 @@ Comandos {
 
 const inputs = `
 <?xml version="1.0"?>
+<project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns="http://maven.apache.org/POM/4.0.0"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <modelVersion>4.0.0</modelVersion>
   <groupId>com.di2win.elis.emission</groupId>
   <artifactId>elis-emission</artifactId>
