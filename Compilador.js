346882180268875caddd6d984cd610b3e1967002
matheusphalves <matheusphalves@gmail.com>
2021-09-02 09:58:08 -0300

    nojeira

--- Compilador.js
+++ Compilador.js
@@ -3,23 +3,23 @@ const ohm = require('ohm-js')
 //A++ to OOA
 const gramatica = ohm.grammar(`
 Comandos {
-    Inicio = Collection+
-    Collection = ("class" ClasseNome ("subclassof" ClasseNome)? "{" Construtor "}") 
-    ClasseNome = letter alnum*
+    Inicio = Colecao+
+    Colecao = "class " ClasseNome "subclassof" ClasseNome " { " Construtor " } " 
+    ClasseNome = letter+
     Construtor = ClasseNome "(" Variavel ( "," Variavel)* ");"
-    Variavel = Tipo letter alnum*
+    Variavel = Tipo Reservada
+    Reservada = ~Tipo letter+
     Tipo = ("int" | "double" | "string" | "long" | "boolean")
 }
 `)
 
 
+
+
 const inputs = `
-class Ponto subclassof Figura {
+class Ponto subclassof Birilo {
     Ponto(int x, int y);
 }
-class PontoA subclassof Figura {
-  PontoA(int x, int y);
-  }
 `;
 
 const resultado = gramatica.match(inputs);
@@ -37,14 +37,40 @@ const ooaCode = ''
 function compile(){
     semantica.addOperation('compile', {
         Inicio(colecao){
-          colecao
+          colecao.compile();
         },
+        Colecao(class_, classeNome, subclassof, classeNome2, ac, construtor, fc){
+          console.log(classeNome2.sourceString)
+          if(classeNome.sourceString == classeNome2.sourceString){
+            throw Error(`A '${classeNome}' não pode herdar dela mesma.`)
+          }
+
+        },
+
+        ClasseNome(nome){
+          console.log(nome.sourceString)
+          return nome.sourceString
+        },
+
+        Construtor(classeNome, ap, variavel1, outrasVariaveis, fp, pv){//Construtor = ClasseNome "(" Variavel ( "," Variavel)* ");"
+          
+        },
+
+        Variavel(tipo, nome){
+          return tipo.compile() + nome.compile();
+        },
+
+        Reservada(nome){
+          return nome.sourceString;
+        },
+
+        Tipo(tipo){return tipo.sourceString;},
+
+        _terminal() {return this.primitiveValue;}
 
     })
 }
 
-/*
-class Ponto subclassof Figura {
-Ponto(int x, int y);
-}
-*/
+compile();
+const execute = semantica(resultado).compile();
+//console.log(lista);
