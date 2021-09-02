0f7bdaed0fdfa7d34f40f27512c97786a5aaf02b
Nilton Vieira <nnilton.vieira@gmail.com>
2021-09-02 10:55:18 -0300

    odio

--- Compilador.js
+++ Compilador.js
@@ -3,10 +3,10 @@ const ohm = require('ohm-js')
 //A++ to OOA
 const gramatica = ohm.grammar(`
 Comandos {
-    Inicio = Collection+
-    Collection = ("class" ClasseNome ("subclassof" ClasseNome)? "{" Construtor "}") 
-    ClasseNome = letter alnum*
-    Construtor = ClasseNome "(" Variavel ( "," Variavel)* ");"
+    Inicio = Colecao+
+    Colecao = "class " classeNome "subclassof" classeNome "{" Construtor "}"
+    classeNome = letter alnum*
+    Construtor = classeNome "(" Variavel ( "," Variavel)* ");"
     Variavel = Tipo letter alnum*
     Tipo = ("int" | "double" | "string" | "long" | "boolean")
 }
@@ -19,7 +19,8 @@ class Ponto subclassof Figura {
 }
 class PontoA subclassof Figura {
   PontoA(int x, int y);
-  }
+}
+
 `;
 
 const resultado = gramatica.match(inputs);
@@ -34,15 +35,38 @@ if(resultado.succeeded()){
 const semantica = gramatica.createSemantics();
 const ooaCode = ''
 
-function compile(){
+/*function compile(){
     semantica.addOperation('compile', {
         Inicio(colecao){
           colecao
         },
 
     })
-}
+}*/
 
+var codigoGerado = "";
+var list = []
+
+function compile(){
+    semantica.addOperation('generateCode', {
+        Inicio(colecao){
+          colecao.generateCode()
+          for (var cont = 0; cont < list.length; cont++) {
+            codigoGerado += list[cont]
+          }
+        },
+        Colecao(class_, classeNome, subClass, nomeClass, ac, constr, fc){
+          var string = class_.sourceString + classeNome.sourceString + " extends " + nomeClass.sourceString + ac.sourceString + "\n\t" + constr.generateCode() + "\n" + fc.sourceString +"\n"
+          list.push(string)
+        },
+        Construtor(classNome, ap, variavel, variaveis, _, fp){
+          return "constructor " + ap.sourceString + variavel.sourceString + variaveis.sourceString + fp.sourceString
+        }
+    })
+}
+compile()
+semantica(resultado).generateCode()
+console.log(codigoGerado)
 /*
 class Ponto subclassof Figura {
 Ponto(int x, int y);
