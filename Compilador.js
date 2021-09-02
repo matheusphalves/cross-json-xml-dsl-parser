7f0233da4f2ef9d58e2acb132864a445c45e4c7c
Matheus Phelipe <matheusphalves@gmail.com>
2021-09-02 14:20:23 -0300

    added fix operations

--- Compilador.js
+++ Compilador.js
@@ -1,3 +1,9 @@
+/*
+1ª AVALIAÇÃO
+DISCIPLINA: COMPILADORES (CCMP006)
+ALUNO: MATHEUS PHELIPE ALVES PINTO
+DATA: 02/09/2021
+*/
 const ohm = require('ohm-js')
 
 //A++ to OOA
@@ -18,11 +24,11 @@ const inputs = `
 class Ponto subclassof Birilo {
     Ponto(int x, int y, int z);
 }
-class Ponto2 subclassof Birilo {
-  Ponto2(int a, int b, int y);
+class Arvore subclassof Birilo {
+  Arvore(int a, int b, int y);
 }
-class Ponto3 subclassof Birilo {
-  Ponto3(int a, int b, int y);
+class Compilador subclassof Birilo {
+  Compilador(int a, int b, double teste);
 }
 `;
 
@@ -40,19 +46,20 @@ const semantica = gramatica.createSemantics();
 function compile(){
     semantica.addOperation('compile', {
         Inicio(colecao){
+          console.log('[COMPILANDO]')
           let colecaoLista = colecao.compile();
           let colecaoSet = new Set(colecaoLista);
           if(colecaoLista.length != colecaoSet.size){
-            throw Error(`Existem classes duplicadas na coleção.`);
+            throw Error(`FALHA: Existem classes duplicadas na coleção.`);
           }
           
         },
         Colecao(class_, classeNome, subclassof, classeNome2, ac, construtor, fc){
           const nomeConstrutor = construtor.compile();
           if(classeNome.sourceString == classeNome2.sourceString){
-            throw Error(`A classe '${classeNome.sourceString}' não pode herdar dela mesma.`)
+            throw Error(`FALHA: A classe '${classeNome.sourceString}' não pode herdar dela mesma.`)
           }else if(nomeConstrutor != classeNome.sourceString){
-            throw Error(`O nome do construtor deve ter o mesmo nome da classe.`)
+            throw Error(`FALHA: O nome do construtor deve ter o mesmo nome da classe.`)
           }
 
           return classeNome.sourceString;
@@ -63,21 +70,21 @@ function compile(){
         },
 
         Construtor(classeNome, ap, variavel1, outrasVariaveis, fp){
-          let variaveisSet = new Set(variavel1.compile().split(' ')[1]);
+          let variaveisSet = new Set(variavel1.compile());
           let count = 1;
           outrasVariaveis.children.map(item => {
-            variaveisSet.add(item.compile().split(' ')[1]);
+            variaveisSet.add(item.compile());
             count++;
           });
           if(variaveisSet.size != count){
-            throw Error(`Construtor possui variáveis duplicadas em sua assinatura.`)
+            throw Error(`FALHA: Construtor possui variáveis duplicadas em sua assinatura.`)
           }
           return classeNome.sourceString;
 
         },
 
         Variavel(tipo, nome){
-          return tipo.compile() + " " + nome.sourceString;
+          return nome.sourceString;
         },
 
         Tipo(tipo){return tipo.sourceString;},
@@ -91,12 +98,13 @@ function compile(){
     })
 }
 
-var codigoGerado = "CÓDIGO GERADO PARA LINGUAGEM OOA\n\n";
+var codigoGerado = "CÓDIGO GERADO PARA LINGUAGEM OOA\n-----------------------------------\n";
 var list = []
 
 function generateCode(){
     semantica.addOperation('generateCode', {
         Inicio(colecao){
+          console.log('[GERANDO CÓDIGO]')
           colecao.generateCode()
           for (var cont = 0; cont < list.length; cont++) {
             codigoGerado += list[cont]
