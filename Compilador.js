cee2ad5ccb2d70a1e865fcaa3e7510ae81f790ad
Murilo Stodolni <m_campanhol@hotmail.com>
2021-09-05 15:33:08 -0300

    TÁ QUEBRADO!!!!!

--- Compilador.js
+++ Compilador.js
@@ -15,194 +15,24 @@ Comandos {
 
 const inputs = `
 <?xml version="1.0"?>
-<modelVersion>4.0.0</modelVersion>
-  <groupId>com.di2win.elis.emission</groupId>
-  <artifactId>elis-emission</artifactId>
-  <version>1.0.0-SNAPSHOT</version>
-  <properties>
-    <compiler-plugin.version>3.8.1</compiler-plugin.version>
-    <maven.compiler.parameters>true</maven.compiler.parameters>
-    <maven.compiler.source>11</maven.compiler.source>
-    <maven.compiler.target>11</maven.compiler.target>
-    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-    <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
-    <quarkus-plugin.version>1.12.2.Final</quarkus-plugin.version>
-    <quarkus.platform.artifact-id>quarkus-universe-bom</quarkus.platform.artifact-id>
-    <quarkus.platform.group-id>io.quarkus</quarkus.platform.group-id>
-    <quarkus.platform.version>1.12.2.Final</quarkus.platform.version>
-    <surefire-plugin.version>2.22.1</surefire-plugin.version>
-  </properties>
-  <dependencyManagement>
-    <dependencies>
-      <dependency>
-        <groupId>{quarkus.platform.group-id}</groupId>
-        <artifactId>{quarkus.platform.artifact-id}</artifactId>
-        <version>{quarkus.platform.version}</version>
-        <type>pom</type>
-        <scope>import</scope>
-      </dependency>
-    </dependencies>
-  </dependencyManagement>
-  <dependencies>
-    <dependency>
+<build>
+  <plugins>
+    <plugin>
       <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-resteasy</artifactId>
-    </dependency>
-    <dependency>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-arc</artifactId>
-    </dependency>
-    <dependency>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-smallrye-reactive-messaging-amqp</artifactId>
-    </dependency>
-    <dependency>
-      <groupId>com.di2win.elis</groupId>
-      <artifactId>elis-commons</artifactId>
-      <version>1.0.0-SNAPSHOT</version>
-    </dependency>
-    <dependency>
-		  <groupId>com.di2win.elis</groupId>
-		  <artifactId>elis-orchestrator-commons</artifactId>
-		  <version>1.0.0-SNAPSHOT</version>
-	  </dependency>
-    <dependency>
-      <groupId>com.di2win.elis</groupId>
-      <artifactId>elis-database</artifactId>
-      <version>1.0.0-SNAPSHOT</version>
-    </dependency>
-    <dependency>
-        <groupId>io.quarkus</groupId>
-        <artifactId>quarkus-hibernate-orm-panache</artifactId>
-    </dependency>
-    <dependency>
-        <groupId>io.quarkus</groupId>
-        <artifactId>quarkus-hibernate-orm</artifactId>
-    </dependency>
-    <dependency>
-        <groupId>io.quarkus</groupId>
-        <artifactId>quarkus-jdbc-mssql</artifactId>
-    </dependency>
-    <dependency>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-resteasy-jsonb</artifactId>
-    </dependency>
-    <dependency>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-smallrye-openapi</artifactId>
-    </dependency>
-    <dependency>
-      <groupId>org.jboss.resteasy</groupId>
-      <artifactId>resteasy-multipart-provider</artifactId>
-    </dependency>
-    <dependency>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-junit5</artifactId>
-      <scope>test</scope>
-    </dependency>
-    <dependency>
-    <groupId>com.googlecode.json-simple</groupId>
-    <artifactId>json-simple</artifactId>
-    <version>1.1.1</version>
-    </dependency>
-    <dependency>
-      <groupId>io.rest-assured</groupId>
-      <artifactId>rest-assured</artifactId>
-      <scope>test</scope>
-    </dependency>
-    <dependency>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-rest-client</artifactId>
-    </dependency>
-    <dependency>
-      <groupId>io.quarkus</groupId>
-      <artifactId>quarkus-smallrye-jwt</artifactId>
-    </dependency>
-    <dependency>
-      <groupId>com.azure</groupId>
-      <artifactId>azure-core</artifactId>
-      <version>1.13.0</version>
-    </dependency>
-    <dependency>
-      <groupId>com.azure</groupId>
-      <artifactId>azure-messaging-servicebus</artifactId>
-      <version>7.0.2</version>
-    </dependency>
-    <dependency>
-    <groupId>org.jboss.logmanager</groupId>
-    <artifactId>log4j2-jboss-logmanager</artifactId>
-</dependency>
-  </dependencies>
-  <build>
-    <plugins>
-      <plugin>
-        <groupId>io.quarkus</groupId>
-        <artifactId>quarkus-maven-plugin</artifactId>
-        <version>{quarkus-plugin.version}</version>
-        <extensions>true</extensions>
-        <executions>
-          <execution>
-            <goals>
-              <goal>build</goal>
-              <goal>generate-code</goal>
-              <goal>generate-code-tests</goal>
-            </goals>
-          </execution>
-        </executions>
-      </plugin>
-      <plugin>
-        <artifactId>maven-compiler-plugin</artifactId>
-        <version>{compiler-plugin.version}</version>
-      </plugin>
-      <plugin>
-        <artifactId>maven-surefire-plugin</artifactId>
-        <version>{surefire-plugin.version}</version>
-        <configuration>
-          <systemPropertyVariables>
-            <java.util.logging.manager>org.apache.logging.log4j.LogManager</java.util.logging.manager>
-            <maven.home>{maven.home}</maven.home>
-          </systemPropertyVariables>
-        </configuration>
-      </plugin>
-    </plugins>
-  </build>
-  <profiles>
-    <profile>
-      <id>native</id>
-      <activation>
-        <property>
-          <name>native</name>
-        </property>
-      </activation>
-      <build>
-        <plugins>
-          <plugin>
-            <artifactId>maven-failsafe-plugin</artifactId>
-            <version>{surefire-plugin.version}</version>
-            <executions>
-              <execution>
-                <goals>
-                  <goal>integration-test</goal>
-                  <goal>verify</goal>
-                </goals>
-                <configuration>
-                  <systemPropertyVariables>
-                    <native.image.path>{project.build.directory}/{project.build.finalName}-runner</native.image.path>
-                    <java.util.logging.manager>org.jboss.logmanager.LogManager</java.util.logging.manager>
-                    <maven.home>{maven.home}</maven.home>
-                  </systemPropertyVariables>
-                </configuration>
-              </execution>
-            </executions>
-          </plugin>
-        </plugins>
-      </build>
-      <properties>
-        <quarkus.package.type>native</quarkus.package.type>
-      </properties>
-    </profile>
-  </profiles>
-
+      <artifactId>quarkus-maven-plugin</artifactId>
+      <version>{quarkus-plugin.version}</version>
+    </plugin>
+    <plugin>
+      <artifactId>maven-compiler-plugin</artifactId>
+      <version>{compiler-plugin.version}</version>
+    </plugin>
+    <plugin>
+      <artifactId>maven-surefire-plugin</artifactId>
+      <version>{surefire-plugin.version}</version>
+    </plugin>
+  </plugins>
+</build>
+ 
 `;
 
 const resultado = gramatica.match(inputs);
@@ -218,7 +48,11 @@ const semantica = gramatica.createSemantics();
 
 var json = "{ \n\t";
 var list = [];
-var contTab = 1
+var contTab = 1;
+//var array = "";
+var tamanhoArray = 0;
+var isArray = false;
+var somaArray = 0;
 
 function backEnd() {
   semantica.addOperation('generateCode', {
@@ -230,65 +64,172 @@ function backEnd() {
       json = json.substring(0, json.length - 1)
       json += "}"
     },
-    Estrutura(start1, variavel, end1, content, start2, variavel2, end2) {
-      if (list.length != 0) {
-        var auxVirgula = list.pop()
-        if (auxVirgula.substring(0,1)=="}") {
-          list.push(auxVirgula.substring(0,1)+ ","+ auxVirgula.substring(1,auxVirgula.length))
-        }else{
-          list.push(auxVirgula)
-        }
-        
+
+
+    Estrutura(start1, variavelAtual, end1, content, start2, variavel2, end2) {
+      console.log("---------------------")
+
+      var estruturaAtual = ""
+
+      console.log("TAB: " + contTab)
+      for (var c = 0; c < contTab; c++) {
+        //console.log(c)
+        estruturaAtual += "\t"
       }
-      var object = "\"" + variavel.sourceString + "\": ";
-      if (content.sourceString.substring(0, 1) == "<") {
-        //if(content.sourceString.substring(1, content.sourceString.indexOf(">"))==variavel.sourceString){
-        /*if (content.sourceString.includes(variavel.sourceString)) {
-          object += "[\n"
-          contTab++
+
+
+      console.log("VARIAVEL ATUAL -> " + variavelAtual.sourceString)
+      console.log("PRÓXIMO CONTENT -> " + content.sourceString)
+      var informacaoFutura = content.sourceString;
+      indexInicio = informacaoFutura.indexOf("<");
+      indexFinal = informacaoFutura.indexOf(">");
+
+      //se tem uma variavel por vir!
+      if(indexInicio != -1 && indexFinal != -1){
+        var proximaVariavel = informacaoFutura.substring(indexInicio + 1, indexFinal);
+        var quantArrayProximaVariavel = (informacaoFutura.split("<" + proximaVariavel + ">").length - 1);
+        console.log("PROXIMA VARIAVEL -> " + proximaVariavel)
+        console.log("TAMANHO: " + quantArrayProximaVariavel)
+
+        //essa variavel por vir é um array?
+        if(quantArrayProximaVariavel >= 2){
+          console.log("É ARRAY COM TAMANHO: " + quantArrayProximaVariavel)
+          estruturaAtual += "\"" + variavelAtual.sourceString + "\": {\n";
+          list.push(estruturaAtual);
+          isArray = true;
+          contTab++;
+          console.log(estruturaAtual)
+          content.generateCode();
+          /*
+          console.log("if if else")
+          var arrayJson = variavelAtual + ":[";
+
+          arrayInicio = str.indexOf("<" + proximaVariavel + ">");
+          arrayFinal = str.indexOf("</" + proximaVariavel + ">");
+          content.children.map(x => {
+            arrayJson += "\n{";
+            //arrayJson += str.substring(variavelAtual.length + 2, arrayFinal);
+            console.log("generateCode")
+            arrayJson += content.generateCode();
+            arrayJson += "},";
+          });
+          
+          console.log(arrayJson)*/
+
+        } else {
+          console.log("É NÃO ARRAY")
+          
+          if(isArray == true){
+            console.log("ARRAY TRUE")
+            console.log("VARIAVEL ATUAL -> " + variavelAtual.sourceString)
+            console.log("PRÓXIMO CONTENT -> " + content.sourceString)
+            content.children.map(x => {
+              console.log(x. )
+            });
+            //estruturaAtual += "\"" + variavelAtual.sourceString + "\": [\n";
+            //list.push(estruturaAtual);
+            //cabecalhoImpresso = true;
+            //console.log(estruturaAtual)
+            //contTab++;
+            //content.generateCode();
+
+          }
+
+          if(tamanhoArray > 0){
+            var infoArray = "";
+            for (var c = 0; c < contTab; c++) {
+              //console.log(c)
+              infoArray += "\t"
+            }
+            tamanhoArray--;
+            console.log("ARRAY: " + tamanhoArray);
+            console.log(estruturaAtual)
+            //contTab++;
+            //content.generateCode();
+          } else {
+            cabecalhoImpresso = false;          
+            console.log("CABECALHO ARRAY NEM TAMANHO ARRAY")
+            estruturaAtual += "\"" + variavelAtual.sourceString + "\": {\n";
+            list.push(estruturaAtual);
+            console.log(estruturaAtual)
+            contTab++;
+            content.generateCode();
+          }
+
+        }
+      } else {
+        console.log("FOLHA")
+        estruturaAtual += "\"" + variavelAtual.sourceString + "\": \"" + content.sourceString + "\",\n";
+        contTab++;
+        list.push(estruturaAtual);
+        console.log(estruturaAtual)
+        content.generateCode();
+      }
+
+      contTab--
+
+        //NILTON
+        /*if (list.length != 0) {
+          var auxVirgula = list.pop()
+          if (auxVirgula.substring(0,1)=="}") {
+            list.push(auxVirgula.substring(0,1)+ ","+ auxVirgula.substring(1,auxVirgula.length))
+          }else{
+            list.push(auxVirgula)
+          }
+          
+        }
+        var object = "\"" + variavel.sourceString + "\": ";
+        if (content.sourceString.substring(0, 1) == "<") {
+          //if(content.sourceString.substring(1, content.sourceString.indexOf(">"))==variavel.sourceString){
+          /*if (content.sourceString.includes(variavel.sourceString)) {
+            object += "[\n"
+            contTab++
+            for (var c = 0; c < contTab; c++) {
+              object += "\t"
+            }
+            list.push(object);
+            content.generateCode();
+            var aux = list.pop()
+            list.push(aux.replace(",", " ").substring(0, aux.length - 1))
+            contTab--
+            var chave = "]\n"
+            for (var c = 0; c < contTab; c++) {
+              chave += "\t"
+            }
+            list.push(chave)
+          }
+          else {
+            object += "{\n"
+            contTab++
+            for (var c = 0; c < contTab; c++) {
+              object += "\t"
+            }
+            list.push(object);
+            content.generateCode();
+            var aux = list.pop()
+            list.push(aux.replace(",", " ").substring(0, aux.length - 1))
+            contTab--
+            var chave = "}\n"
+            for (var c = 0; c < contTab; c++) {
+              chave += "\t"
+            }
+            list.push(chave)
+          //}
+          //}
+  
+  
+        }
+        else {
+          var conteudo = content.generateCode();
+          object += conteudo
           for (var c = 0; c < contTab; c++) {
             object += "\t"
           }
-          list.push(object);
-          content.generateCode();
-          var aux = list.pop()
-          list.push(aux.replace(",", " ").substring(0, aux.length - 1))
-          contTab--
-          var chave = "]\n"
-          for (var c = 0; c < contTab; c++) {
-            chave += "\t"
-          }
-          list.push(chave)
+          list.push(object)
         }
-        else {*/
-          object += "{\n"
-          contTab++
-          for (var c = 0; c < contTab; c++) {
-            object += "\t"
-          }
-          list.push(object);
-          content.generateCode();
-          var aux = list.pop()
-          list.push(aux.replace(",", " ").substring(0, aux.length - 1))
-          contTab--
-          var chave = "}\n"
-          for (var c = 0; c < contTab; c++) {
-            chave += "\t"
-          }
-          list.push(chave)
-        //}
-        //}
+      */
 
-
-      }
-      else {
-        var conteudo = content.generateCode();
-        object += conteudo
-        for (var c = 0; c < contTab; c++) {
-          object += "\t"
-        }
-        list.push(object)
-      }
+    //console.log("\n---------------\nRESULT: " + mySubString + " -> " + (str.split(mySubString).length - 1) +"\n---------------")
     },
     Informacao(info) {
       return "\"" + info.sourceString + "\",\n"
@@ -322,4 +263,5 @@ backEnd();
 //compile();
 //semantica(resultado).compile()
 semantica(resultado).generateCode()
+console.log("------------------- JSON PRONTO -------------------")
 console.log(json)
